a499ba761889384879c73a8e87fb356fa513c0df
safvanar <safvananiyaram@gmail.com>
2023-09-18 15:15:23 +0530

    start with dom-JS

---
+++ dom.js
@@ -0,0 +1,15 @@
+// console.dir(document);
+// console.log(document.URL)
+// console.log(document.domain)
+// console.log(document.title)
+// document.title='Title changed'
+// console.log(document.all)
+var header=document.getElementById("main-header")
+header.style.borderBottom="solid 3px #000"
+var title=document.getElementById("mainTitle")
+title.style.fontWeight="bold"
+title.style.color='green'
+
+
+//GET ELEMENTS BY CLASSNAME:
+
